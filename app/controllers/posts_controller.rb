da6e57048d8888a11d52fb6e81b8e2ff25061b06
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-11-17 00:24:06 +0800

    Updated redirection path to create new order after creating new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -51,7 +51,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to new_post_order_path(@post.id), notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
